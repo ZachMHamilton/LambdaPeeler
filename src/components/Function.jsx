641aafc73e0ce3b8a0cd266e1134deb4376e13c4
Zach Hamilton <zachmhamilton@outlook.com>
2023-09-28 12:33:21 -0400

    final2

--- src/components/Function.jsx
+++ src/components/Function.jsx
@@ -34,7 +34,7 @@ const Function = ({ functionName, ARN, functionLayersARN, layers }) => {
     axios
       //post request to functionRouter.js
       .post(
-        'http://localhost:3000/api/functions/layers',
+        'https://lambda-peeler.onrender.com/api/functions/layers',
         //pass Function ARN and Layers Array to backend
         { ARN: ARN, layers: layers },
         {
